ad8fb9a012c3433ad56f77ea125c04373be414e3
Mpho077 <69971504+Mpho077@users.noreply.github.com>
2026-03-08 09:24:24 +0200

    Delete direct_print directory

--- direct_print/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import models
-from . import wizards
-from . import hooks
-
-
-def _post_init_hook(env):
-    """Called after module installation to create Direct Print actions."""
-    hooks.post_init_hook(env)
-
